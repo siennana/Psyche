676e375851c6ca81393675de8540bc2ef5f38ab2
Sienna Brown <31221385+siennana@users.noreply.github.com>
2020-03-21 01:09:00 -0700

    ideas

---
+++ Assets/Scripts/Ideas.cs
@@ -0,0 +1,8 @@
+﻿/* IDEAS
+ 
+   STORYLINE
+
+   * Character did something horrible (murdered family) and must uncover dark past
+   * alternate personalities
+   * Being watched, in danger, whole time it is self
+*/
--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Story : MonoBehaviour
+public class SceneSwitch : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
