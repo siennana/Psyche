b6e0e1c63ff40c1e5204ac5c3efe8236c8db8eef
Sienna Brown <31221385+siennana@users.noreply.github.com>
2020-05-13 00:51:02 -0700

    attempt at making door responsive..fail

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -1,18 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//Make an empty GameObject and call it "Door"
+//Drag and drop your Door model into Scene and rename it to "Body"
+//Make sure that the "Door" Object is at the side of the "Body" object (The place where a Door Hinge should be)
+//Move the "Body" Object inside "Door"
+//Add a Collider (preferably SphereCollider) to "Door" object and make it much bigger then the "Body" model, mark it as Trigger
+//Assign this script to a "Door" Object (the one with a Trigger Collider)
+//Make sure the main Character is tagged "Player"
+//Upon walking into trigger area press "F" to open / close the door
+
 using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    // Smoothly open a door
+    public float doorOpenAngle = 90.0f; //Set either positive or negative number to open the door inwards or outwards
+    public float openSpeed = 2.0f; //Increasing this value will make the door open faster
+
+    bool open = false;
+    bool enter = false;
+
+    float defaultRotationAngle;
+    float currentRotationAngle;
+    float openTime = 0;
+
     void Start()
     {
-        
+        defaultRotationAngle = transform.localEulerAngles.y;
+        currentRotationAngle = transform.localEulerAngles.y;
     }
 
-    // Update is called once per frame
+    // Main function
     void Update()
     {
-        
+        if (openTime < 1)
+        {
+            openTime += Time.deltaTime * openSpeed;
+        }
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Mathf.LerpAngle(currentRotationAngle, 
+            defaultRotationAngle + (open ? doorOpenAngle : 0), openTime), transform.localEulerAngles.z);
+
+        if (Input.GetKeyDown(KeyCode.F) && enter)
+        {
+            open = !open;
+            currentRotationAngle = transform.localEulerAngles.y;
+            openTime = 0;
+        }
+    }
+
+    // Display a simple info message when player is inside the trigger area
+    void OnGUI()
+    {
+        if (enter)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 75, Screen.height - 100, 150, 30), "Press 'F' to open the door");
+        }
+    }
+
+    // Activate the Main function when Player enter the trigger area
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enter = true;
+        }
+    }
+
+    // Deactivate the Main function when Player exit the trigger area
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enter = false;
+        }
     }
 }
